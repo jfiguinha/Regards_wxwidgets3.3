6a1e7e0599df7c7760262593efe3c9488eb8494d
Figuinha <jfiguinha@hotmail.fr>
2026-05-14 10:04:55 +0200

    Correct bug on checkbox bad display in directory tree

--- libextern/wxconfig_cpp.sh
+++ libextern/wxconfig_cpp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ $OSTYPE == 'darwin'* ]]; then
 	#/opt/homebrew/bin/wx-config --cppflags
-	/Volumes/WD_BLACK/developpement/git/Regards/libextern/vcpkg/installed/arm64-osx/tools/wxwidgets/wx-config --cppflags
+	/Volumes/WD_BLACK/developpement/git/Regards_wxwidgets3.3/libextern/vcpkg/installed/arm64-osx/tools/wxwidgets/wx-config --cppflags
 else
 #	${HOME}/ffmpeg_build/bin/wx-config --cppflags
-    ${HOME}/developpement/git/Regards/libextern/vcpkg/installed/x64-linux/tools/wxwidgets/wx-config --cppflags
+    ${HOME}/developpement/git/Regards_wxwidgets3.3/libextern/vcpkg/installed/x64-linux/tools/wxwidgets/wx-config --cppflags
 #   ${HOME}/developpement/git/Regards/libextern/vcpkg-2022.08.15/installed/x64-linux/tools/wxwidgets/wx-config --cppflags
 #   ${HOME}/developpement/git/Regards/libextern/wxWidgets-3.2.3/build_new/wx-config --cppflags
 fi
