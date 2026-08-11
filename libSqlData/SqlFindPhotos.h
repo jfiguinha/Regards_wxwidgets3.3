99318aee7626233f06e299980737e6a256882a5a
Jacques Figuinha <jfiguinha@hotmail.fr>
2026-08-11 17:02:43 +0200

    Correct bug

--- libSqlData/SqlFindPhotos.h
+++ libSqlData/SqlFindPhotos.h
@@ -37,19 +37,6 @@ namespace Regards
 
 		private:
 
-			wxString GenerateSqlRequest_chatgpt(
-				const int& numCatalog,
-				vector<int>& listFolder,
-				vector<int>& listCriteriaNotIn,
-				vector<int>& listFaceNotIn,
-				vector<int>& listFaceSelected,
-				vector<int>& listStarSelected,
-				vector<int>& listStarNotSelected,
-				vector<int>& listKeywordSelected,
-				vector<int>& listKeywordNotSelected,
-				const wxString& libelleNotGeo,
-				const double& pertinence);
-
 			bool FindIfViewExist();
 			int TraitementResultPhotoDataCriteria(CSqlResult* sqlResult);
 			int TraitementResultPhoto(CSqlResult* sqlResult);
